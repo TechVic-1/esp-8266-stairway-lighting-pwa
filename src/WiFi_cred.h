c34912aa8378f9ddb78cbcd51547be0870d8fb4b
TechVic <victor.martinsson@hotmail.com>
2022-03-26 16:18:49 +0100

    Update WiFi_cred.h

--- src/WiFi_cred.h
+++ src/WiFi_cred.h
@@ -1,5 +1,12 @@
 
 
-String WIFI_SSID = "***";
-String WIFI_PASS = "***";
+String WIFI_SSID = "*******";
+String WIFI_PASS = "*******";
 #define WIFI_HOSTNAME "Stairway-Led"
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 50, 122);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 50, 1);
+
+IPAddress subnet(255, 255, 255, 0);
